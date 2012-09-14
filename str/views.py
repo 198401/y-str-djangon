3c3ff9f8fd2ca973f9ec7518becf8cf54da30790
af <198401@gmail.com>
2012-09-14 15:58:24 +0800

    fix db bug

--- str/views.py
+++ str/views.py
@@ -13,20 +13,25 @@ def index(request):
         f = SearchForm(request.POST)
         if f.is_valid():
             cd = f.cleaned_data
-            if not cd['str1'] and not cd['str2'] \
-               and not cd['str3'] and not cd['str4'] \
-               and not cd['str5'] and not cd['str6'] \
-               and not cd['str7'] and not cd['str8'] \
-               and not cd['str9'] and not cd['str10'] \
-               and not cd['str11'] and not cd['str12'] \
-               and not cd['str13'] and not cd['str14'] \
-               and not cd['str15'] and not cd['str16'] \
-	       and not cd['str17'] and not cd['str18'] \
-	       and not cd['str19'] and not cd['str20'] \
-	       and not cd['str21'] and not cd['str22'] \
-	       and not cd['str23'] and not cd['str24'] \
-	       and not cd['str37'] \
-            and not cd['str38']:
+            if not cd['DYS388'] and not cd['DYS19'] \
+               and not cd['DYS389I'] and not cd['DYS389II'] \
+               and not cd['DYS390'] and not cd['DYS391'] \
+               and not cd['DYS392'] and not cd['DYS393'] \
+               and not cd['DYS425'] and not cd['DYS426'] \
+               and not cd['DYS434'] and not cd['DYS435'] \
+               and not cd['DYS436'] and not cd['DYS437'] \
+               and not cd['DYS438'] and not cd['DYS439'] \
+	       and not cd['DYS385a'] and not cd['DYS385b'] \
+	       and not cd['DYSA1_2'] and not cd['YGATAH4'] \
+	       and not cd['DYS448'] and not cd['DYS456'] \
+	       and not cd['DYS458'] and not cd['DYS635'] \
+	       and not cd['YCAIIa'] and not cd['YCAIIb'] \
+	       and not cd['DYS594'] and not cd['DYS596'] \
+	       and not cd['DYSA1_2'] and not cd['YGATAH4'] \
+	       and not cd['YPENTA1'] and not cd['YPENTA2'] \
+	       and not cd['DYS643'] and not cd['DYS645'] \
+	       and not cd['DYF411S1a'] \
+            and not cd['DYF411S1b']:
                 error = u'未输入搜索条件'
                 return render_to_response('base.html', locals())
             else:
@@ -34,110 +39,142 @@ def index(request):
                 a=[]
 		b=[]
 		result=[]
-                if cd['str1'] :
-                    ssq = ssq.filter(str1__gte = 0)
-		    a.append(int(cd['str1']))
-		    b.append('str1')
-		if cd['str2'] :
-                    ssq = ssq.filter(str2__gte = 0)
-		    a.append(int(cd['str2']))
-		    b.append('str2')
-		if cd['str3'] :
-                    ssq = ssq.filter(str3__gte = 0)
-                    a.append(int(cd['str3']))
-                    b.append('str3')
-                if cd['str4'] :
-		    ssq = ssq.filter(str4__gte = 0)
-		    a.append(int(cd['str4']))
-		    b.append('str4')
-		if cd['str5'] :
-                    ssq = ssq.filter(str5__gte = 0)
-		    a.append(int(cd['str5']))
-		    b.append('str5')
-		if cd['str6'] :
-                    ssq = ssq.filter(str6__gte = 0)
-		    a.append(int(cd['str6']))
-		    b.append('str6')
-		if cd['str7'] :
-                    ssq = ssq.filter(str7__gte = 0)
-		    a.append(int(cd['str7']))
-		    b.append('str7')
-		if cd['str8'] :
-                    ssq = ssq.filter(str8__gte = 0)
-		    a.append(int(cd['str8']))
-		    b.append('str8')
-		if cd['str9'] :
-                    ssq = ssq.filter(str9__gte = 0)
-                    a.append(int(cd['str9']))
-                    b.append('str9')
-		if cd['str10'] :
-                    ssq = ssq.filter(str10__gte = 0)
-		    a.append(int(cd['str10']))
-		    b.append('str10')
-		if cd['str11'] :
-                    ssq = ssq.filter(str11__gte = 0)
-		    a.append(int(cd['str11']))
-		    b.append('str11')
-		if cd['str12'] :
-                    ssq = ssq.filter(str12__gte = 0)
-                    a.append(int(cd['str12']))
-                    b.append('str12')
-		if cd['str13'] :
-                    ssq = ssq.filter(str13__gte = 0)
-		    a.append(int(cd['str13']))
-		    b.append('str13')
-		if cd['str14'] :
-                    ssq = ssq.filter(str14__gte = 0)
-		    a.append(int(cd['str14']))
-		    b.append('str14')
-		if cd['str15'] :
-                    ssq = ssq.filter(str15__gte = 0)
-                    a.append(int(cd['str15']))
-                    b.append('str15')
-		if cd['str16'] :
-                    ssq = ssq.filter(str16__gte = 0)
-		    a.append(int(cd['str16']))
-		    b.append('str16')
-		if cd['str17'] :
-                    ssq = ssq.filter(str17__gte = 0)
-		    a.append(int(cd['str17']))
-		    b.append('str17')
-		if cd['str18'] :
-                    ssq = ssq.filter(str18__gte = 0)
-                    a.append(int(cd['str18']))
-                    b.append('str18')
-		if cd['str19'] :
-                    ssq = ssq.filter(str19__gte = 0)
-		    a.append(int(cd['str19']))
-		    b.append('str19')
-		if cd['str20'] :
-                    ssq = ssq.filter(str20__gte = 0)
-		    a.append(int(cd['str20']))
-		    b.append('str20')
-		if cd['str21'] :
-                    ssq = ssq.filter(str21__gte = 0)
-                    a.append(int(cd['str21']))
-                    b.append('str21')
-		if cd['str22'] :
-                    ssq = ssq.filter(str22__gte = 0)
-		    a.append(int(cd['str22']))
-		    b.append('str22')
-		if cd['str23'] :
-                    ssq = ssq.filter(str23__gte = 0)
-		    a.append(int(cd['str23']))
-		    b.append('str23')
-		if cd['str24'] :
-                    ssq = ssq.filter(str24__gte = 0)
-                    a.append(int(cd['str24']))
-                    b.append('str34')
-		if cd['str37'] :
-                    ssq = ssq.filter(str37__gte = 0)
-		    a.append(int(cd['str37']))
-		    b.append('str37')
-		if cd['str38'] :
-                    ssq = ssq.filter(str38__gte = 0)
-                    a.append(int(cd['str38']))
-                    b.append('str38')
+                if cd['DYS388'] :
+                    ssq = ssq.filter(DYS388__gte = 0)
+		    a.append(int(cd['DYS388']))
+		    b.append('DYS388')
+		if cd['DYS19'] :
+                    ssq = ssq.filter(DYS19__gte = 0)
+		    a.append(int(cd['DYS19']))
+		    b.append('DYS19')
+		if cd['DYS389I'] :
+                    ssq = ssq.filter(DYS389I__gte = 0)
+                    a.append(int(cd['DYS389I']))
+                    b.append('DYS389I')
+                if cd['DYS389II'] :
+		    ssq = ssq.filter(DYS389II__gte = 0)
+		    a.append(int(cd['DYS389II']))
+		    b.append('DYS389II')
+		if cd['DYS390'] :
+                    ssq = ssq.filter(DYS390__gte = 0)
+		    a.append(int(cd['DYS390']))
+		    b.append('DYS390')
+		if cd['DYS391'] :
+                    ssq = ssq.filter(DYS391__gte = 0)
+		    a.append(int(cd['DYS391']))
+		    b.append('DYS391')
+		if cd['DYS392'] :
+                    ssq = ssq.filter(DYS392__gte = 0)
+		    a.append(int(cd['DYS392']))
+		    b.append('DYS392')
+		if cd['DYS393'] :
+                    ssq = ssq.filter(DYS393__gte = 0)
+		    a.append(int(cd['DYS393']))
+		    b.append('DYS393')
+		if cd['DYS425'] :
+                    ssq = ssq.filter(DYS425__gte = 0)
+                    a.append(int(cd['DYS425']))
+                    b.append('DYS425')
+		if cd['DYS426'] :
+                    ssq = ssq.filter(DYS426__gte = 0)
+		    a.append(int(cd['DYS426']))
+		    b.append('DYS426')
+		if cd['DYS434'] :
+                    ssq = ssq.filter(DYS434__gte = 0)
+		    a.append(int(cd['DYS434']))
+		    b.append('DYS434')
+		if cd['DYS435'] :
+                    ssq = ssq.filter(DYS435__gte = 0)
+                    a.append(int(cd['DYS435']))
+                    b.append('DYS435')
+		if cd['DYS436'] :
+                    ssq = ssq.filter(DYS436__gte = 0)
+		    a.append(int(cd['DYS436']))
+		    b.append('DYS436')
+		if cd['DYS437'] :
+                    ssq = ssq.filter(DYS437__gte = 0)
+		    a.append(int(cd['DYS437']))
+		    b.append('DYS437')
+		if cd['DYS438'] :
+                    ssq = ssq.filter(DYS438__gte = 0)
+                    a.append(int(cd['DYS438']))
+                    b.append('DYS438')
+		if cd['DYS439'] :
+                    ssq = ssq.filter(DYS439__gte = 0)
+		    a.append(int(cd['DYS439']))
+		    b.append('DYS439')
+		if cd['DYS385a'] :
+                    ssq = ssq.filter(DYS385a__gte = 0)
+		    a.append(int(cd['DYS385a']))
+		    b.append('DYS385a')
+		if cd['DYS385b'] :
+                    ssq = ssq.filter(DYS385b__gte = 0)
+                    a.append(int(cd['DYS385b']))
+                    b.append('DYS385b')
+		if cd['DYSA1_2'] :
+                    ssq = ssq.filter(DYSA1_2__gte = 0)
+		    a.append(int(cd['DYSA1_2']))
+		    b.append('DYSA1_2')
+		if cd['YGATAH4'] :
+                    ssq = ssq.filter(YGATAH4__gte = 0)
+		    a.append(int(cd['YGATAH4']))
+		    b.append('YGATAH4')
+		if cd['DYS448'] :
+                    ssq = ssq.filter(DYS448__gte = 0)
+                    a.append(int(cd['DYS448']))
+                    b.append('DYS448')
+		if cd['DYS456'] :
+                    ssq = ssq.filter(DYS456__gte = 0)
+		    a.append(int(cd['DYS456']))
+		    b.append('DYS456')
+		if cd['DYS458'] :
+                    ssq = ssq.filter(DYS458__gte = 0)
+		    a.append(int(cd['DYS458']))
+		    b.append('DYS458')
+		if cd['DYS635'] :
+                    ssq = ssq.filter(DYS635__gte = 0)
+                    a.append(int(cd['DYS635']))
+                    b.append('DYS635')
+		if cd['YCAIIa'] :
+                    ssq = ssq.filter(YCAIIa__gte = 0)
+		    a.append(int(cd['YCAIIa']))
+		    b.append('YCAIIa')
+		if cd['YCAIIb'] :
+                    ssq = ssq.filter(YCAIIb__gte = 0)
+                    a.append(int(cd['YCAIIb']))
+                    b.append('YCAIIb')
+                if cd['DYS594'] :
+                    ssq = ssq.filter(DYS594__gte = 0)
+                    a.append(int(cd['DYS594']))
+                    b.append('DYS594')
+                if cd['DYS596'] :
+                    ssq = ssq.filter(DYS596__gte = 0)
+                    a.append(int(cd['DYS596']))
+                    b.append('DYS596')
+                if cd['YPENTA1'] :
+                    ssq = ssq.filter(YPENTA1__gte = 0)
+                    a.append(int(cd['YPENTA1']))
+                    b.append('YPENTA1')
+                if cd['YPENTA2'] :
+                    ssq = ssq.filter(YPENTA2__gte = 0)
+                    a.append(int(cd['YPENTA2']))
+                    b.append('YPENTA2')
+                if cd['DYS643'] :
+                    ssq = ssq.filter(DYS643__gte = 0)
+                    a.append(int(cd['DYS643']))
+                    b.append('DYS643')
+                if cd['DYS645'] :
+                    ssq = ssq.filter(DYS645__gte = 0)
+                    a.append(int(cd['DYS645']))
+                    b.append('DYS645')
+                if cd['DYF411S1a'] :
+                    ssq = ssq.filter(DYF411S1a__gte = 0)
+                    a.append(int(cd['DYF411S1a']))
+                    b.append('DYF411S1a')
+                if cd['DYF411S1b'] :
+                    ssq = ssq.filter(DYF411S1b__gte = 0)
+                    a.append(int(cd['DYF411S1b']))
+                    b.append('DYF411S1b')
 		b.append('steps')
 		result=[]
 		for row in ssq:
@@ -150,109 +187,141 @@ def index(request):
 		    wd.append(row.location)
 		    wd.append(row.SNP)
 		    wd.append(row.HG)
-		    if 'str1' in b:
-			    wd.append(row.str1)
-			    sum+=abs(row.str1-a[i])
+		    if 'DYS388' in b:
+			    wd.append(row.DYS388)
+			    sum+=abs(row.DYS388-a[i])
 			    i+=1
-		    if 'str2' in b:
-			    wd.append(row.str2)
-			    sum+=abs(row.str2-a[i])
+		    if 'DYS19' in b:
+			    wd.append(row.DYS19)
+			    sum+=abs(row.DYS19-a[i])
 			    i+=1
-		    if 'str3' in b:
-			    wd.append(row.str3)
-			    sum+=abs(row.str3-a[i])
+		    if 'DYS389I' in b:
+			    wd.append(row.DYS389I)
+			    sum+=abs(row.DYS389I-a[i])
 			    i+=1
-		    if 'str4' in b:
-			    wd.append(row.str4)
-			    sum+=abs(row.str4-a[i])
+		    if 'DYS389II' in b:
+			    wd.append(row.DYS389II)
+			    sum+=abs(row.DYS389II-a[i])
 			    i+=1
-		    if 'str5' in b:
-			    wd.append(row.str5)
-			    sum+=abs(row.str5-a[i])
+		    if 'DYS390' in b:
+			    wd.append(row.DYS390)
+			    sum+=abs(row.DYS390-a[i])
 			    i+=1
-		    if 'str6' in b:
-			    wd.append(row.str6)
-			    sum+=abs(row.str6-a[i])
+		    if 'DYS391' in b:
+			    wd.append(row.DYS391)
+			    sum+=abs(row.DYS391-a[i])
 			    i+=1
-		    if 'str7' in b:
-			    wd.append(row.str7)
-			    sum+=abs(row.str7-a[i])
+		    if 'DYS392' in b:
+			    wd.append(row.DYS392)
+			    sum+=abs(row.DYS392-a[i])
 			    i+=1
-		    if 'str8' in b:
-			    wd.append(row.str8)
-			    sum+=abs(row.str8-a[i])
+		    if 'DYS393' in b:
+			    wd.append(row.DYS393)
+			    sum+=abs(row.DYS393-a[i])
 			    i+=1
-		    if 'str9' in b:
-			    wd.append(row.str9)
-			    sum+=abs(row.str9-a[i])
+		    if 'DYS425' in b:
+			    wd.append(row.DYS425)
+			    sum+=abs(row.DYS425-a[i])
 			    i+=1
-		    if 'str10' in b:
-			    wd.append(row.str10)
-			    sum+=abs(row.str10-a[i])
+		    if 'DYS426' in b:
+			    wd.append(row.DYS426)
+			    sum+=abs(row.DYS426-a[i])
 			    i+=1
-		    if 'str11' in b:
-			    wd.append(row.str11)
-			    sum+=abs(row.str11-a[i])
+		    if 'DYS434' in b:
+			    wd.append(row.DYS434)
+			    sum+=abs(row.DYS434-a[i])
 			    i+=1
-		    if 'str12' in b:
-			    wd.append(row.str12)
-			    sum+=abs(row.str12-a[i])
+		    if 'DYS435' in b:
+			    wd.append(row.DYS435)
+			    sum+=abs(row.DYS435-a[i])
 			    i+=1
-		    if 'str13' in b:
-			    wd.append(row.str13)
-			    sum+=abs(row.str13-a[i])
+		    if 'DYS436' in b:
+			    wd.append(row.DYS436)
+			    sum+=abs(row.DYS436-a[i])
 			    i+=1
-		    if 'str14' in b:
-			    wd.append(row.str14)
-			    sum+=abs(row.str14-a[i])
+		    if 'DYS437' in b:
+			    wd.append(row.DYS437)
+			    sum+=abs(row.DYS437-a[i])
 			    i+=1
-		    if 'str15' in b:
-			    wd.append(row.str15)
-			    sum+=abs(row.str15-a[i])
+		    if 'DYS438' in b:
+			    wd.append(row.DYS438)
+			    sum+=abs(row.DYS438-a[i])
 			    i+=1
-		    if 'str16' in b:
-			    wd.append(row.str16)
-			    sum+=abs(row.str16-a[i])
+		    if 'DYS439' in b:
+			    wd.append(row.DYS439)
+			    sum+=abs(row.DYS439-a[i])
 			    i+=1
-		    if 'str17' in b:
-			    wd.append(row.str17)
-			    sum+=abs(row.str17-a[i])
+		    if 'DYS385a' in b:
+			    wd.append(row.DYS385a)
+			    sum+=abs(row.DYS385a-a[i])
 			    i+=1
-		    if 'str18' in b:
-			    wd.append(row.str18)
-			    sum+=abs(row.str18-a[i])
+		    if 'DYS385b' in b:
+			    wd.append(row.DYS385b)
+			    sum+=abs(row.DYS385b-a[i])
 			    i+=1
-		    if 'str19' in b:
-			    wd.append(row.str19)
-			    sum+=abs(row.str19-a[i])
+		    if 'DYSA1_2' in b:
+			    wd.append(row.DYSA1_2)
+			    sum+=abs(row.DYSA1_2-a[i])
 			    i+=1
-		    if 'str20' in b:
-			    wd.append(row.str20)
-			    sum+=abs(row.str20-a[i])
+		    if 'YGATAH4' in b:
+			    wd.append(row.YGATAH4)
+			    sum+=abs(row.YGATAH4-a[i])
 			    i+=1
-		    if 'str21' in b:
-			    wd.append(row.str21)
-			    sum+=abs(row.str21-a[i])
+		    if 'DYS448' in b:
+			    wd.append(row.DYS448)
+			    sum+=abs(row.DYS448-a[i])
 			    i+=1
-		    if 'str22' in b:
-			    wd.append(row.str22)
-			    sum+=abs(row.str22-a[i])
+		    if 'DYS456' in b:
+			    wd.append(row.DYS456)
+			    sum+=abs(row.DYS456-a[i])
 			    i+=1
-		    if 'str23' in b:
-			    wd.append(row.str23)
-			    sum+=abs(row.str23-a[i])
+		    if 'DYS458' in b:
+			    wd.append(row.DYS458)
+			    sum+=abs(row.DYS458-a[i])
 			    i+=1
-		    if 'str24' in b:
-			    wd.append(row.str24)
-			    sum+=abs(row.str24-a[i])
+		    if 'DYS635' in b:
+			    wd.append(row.DYS635)
+			    sum+=abs(row.DYS635-a[i])
 			    i+=1
-		    if 'str37' in b:
-			    wd.append(row.str37)
-			    sum+=abs(row.str37-a[i])
+		    if 'YCAIIa' in b:
+			    wd.append(row.YCAIIa)
+			    sum+=abs(row.YCAIIa-a[i])
 			    i+=1
-		    if 'str38' in b:
-			    wd.append(row.str38)
-			    sum+=abs(row.str38-a[i])
+		    if 'YCAIIb' in b:
+			    wd.append(row.YCAIIb)
+			    sum+=abs(row.YCAIIb-a[i])
+			    i+=1
+		    if 'DYS594' in b:
+			    wd.append(row.DYS594)
+			    sum+=abs(row.DYS594-a[i])
+			    i+=1
+		    if 'DYS596' in b:
+			    wd.append(row.DYS596)
+			    sum+=abs(row.DYS596-a[i])
+			    i+=1
+		    if 'YPENTA1' in b:
+			    wd.append(row.YPENTA1)
+			    sum+=abs(row.YPENTA1-a[i])
+			    i+=1
+		    if 'YPENTA2' in b:
+			    wd.append(row.YPENTA2)
+			    sum+=abs(row.YPENTA2-a[i])
+			    i+=1
+		    if 'DYS643' in b:
+			    wd.append(row.DYS643)
+			    sum+=abs(row.DYS643-a[i])
+			    i+=1
+		    if 'DYS645' in b:
+			    wd.append(row.DYS645)
+			    sum+=abs(row.DYS645-a[i])
+			    i+=1
+		    if 'DYF411S1a' in b:
+			    wd.append(row.DYF411S1a)
+			    sum+=abs(row.DYF411S1a-a[i])
+			    i+=1
+		    if 'DYF411S1b' in b:
+			    wd.append(row.DYF411S1b)
+			    sum+=abs(row.DYF411S1b-a[i])
 			    i+=1
 		    if sum<6:
                             wd.append(sum)                         
